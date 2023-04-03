31ef93e8064422633f6bb933fc3ac1c1964683f6
Yusuf Keçer <yusufkecer0007@gmail.com>
2023-04-03 05:16:07 +0300

    Update data_service.dart

--- lib/product/service/data_service.dart
+++ lib/product/service/data_service.dart
@@ -98,24 +98,3 @@ class DataService {
     }
   }
 }
-//  class DataService {
-//   Future<Iterable> fetchData() async {
-//     const url = "https://63c70d76d307b76967462d8b.mockapi.io/workers";
-//     final data = await compute(getWorkers, url);
-//     return data;
-//   }
-
-//   Future getWorkers(url) async {
-//     final http.Response response = await http.get(Uri.parse(url));
-
-//     if (response.statusCode == 200) {
-//       // If the server did return a 200 OK response,
-//       // then parse the JSON.
-//       return jsonDecode(response.body);
-//     } else {
-//       // If the server did not return a 200 OK response,
-//       // then throw an exception.
-//       throw Exception('Failed to load album');
-//     }
-//   }
-// }
