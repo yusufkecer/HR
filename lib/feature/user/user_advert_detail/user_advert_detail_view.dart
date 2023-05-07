125fb726fbe701f57bdc735bb7d2bd75b25c6699
Yusuf Keçer <yusufkecer0007@gmail.com>
2023-05-07 18:15:28 +0300

    update

--- lib/feature/user/user_advert_detail/user_advert_detail_view.dart
+++ lib/feature/user/user_advert_detail/user_advert_detail_view.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-
 import '../../../Product/Constant/colors.dart';
 import '../../../product/models/general_company_model.dart';
 
